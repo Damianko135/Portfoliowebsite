be10829e286c6fae8b5c07b5805b28132a430d7e
Damianko135 <139293484+Damianko135@users.noreply.github.com>
2024-02-09 21:16:51 +0100

    Why does the gitignore not work?

--- Index/Pages/Project_1/Scripts/Connection.php
+++ Index/Pages/Project_1/Scripts/Connection.php
@@ -4,7 +4,7 @@ define('DB_HOST', 'localhost');
 define('DB_PORT', 3306);
 define('DB_NAME', 'portfolio');
 define('DB_USER', 'root');
-define('DB_PASS', '');
+define('DB_PASS', 'poiu');
 
 global $PDO;
 
