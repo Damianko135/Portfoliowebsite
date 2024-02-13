36d406c3163c8ef7fce644bca659bc6c82c503d0
Damianko135 <139293484+Damianko135@users.noreply.github.com>
2024-02-13 20:35:00 +0100

    Update MD.sh

--- MD.sh
+++ MD.sh
@@ -8,24 +8,19 @@ github_repo="https://github.com/Damianko135/Portfoliowebsite.git"
 directory="Index"  # Change this to the directory you want to pull
 
 # Function to handle errors
-handle_error() {
-    local error_message="$1"
-    echo "Error: $error_message" >&2
-    exit 1
-}
+
 
 # Update system packages and upgrade kept back packages
-sudo apt update || handle_error "Failed to update system packages"x
+sudo apt update || echo "Failed to update system packages"x
 sudo apt install git -y
 
-# Check if the destination directory exists, if not, create it
-if [ ! -d "$destination_dir" ]; then
-    sudo mkdir -p "$destination_dir" || handle_error "Failed to create destination directory"
-fi
+sudo rm -rf "$destination_dir"
 
-cd "$destination_dir" || handle_error "Failed to navigate to destination directory"
+sudo mkdir -p "$destination_dir" || echo "Failed to create destination directory"
 
-sudo git clone -b "$branch" --single-branch "$github_repo" . || handle_error "Failed to clone repository"
+cd "$destination_dir" || echo "Failed to navigate to destination directory"
+
+sudo git clone -b "$branch" --single-branch "$github_repo" . || echo "Failed to clone repository"
 
 
 sudo bash /var/www/html/Linux_Setup.sh
