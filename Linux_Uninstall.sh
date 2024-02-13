784af561eb8ab4f0755664204e7257e057e7eba9
Damianko135 <139293484+Damianko135@users.noreply.github.com>
2024-02-13 20:19:50 +0100

    Update Linux_Uninstall.sh

--- Linux_Uninstall.sh
+++ Linux_Uninstall.sh
@@ -7,12 +7,6 @@ Virtual_Host="/var/www/html/Index"
 # Remove permissions reset scheduled after 10 minutes
 ps -ef | grep "sudo chmod -R 755 $destination_dir" | grep -v grep | grep -v $$ | awk '{print $2}' | xargs sudo kill -9
 
-# Undo changes to Apache Virtual Host Configuration
-sudo sed -i "s#DocumentRoot $Virtual_Host#DocumentRoot /var/www/html#" /etc/apache2/sites-available/000-default.conf
-sudo sed -i '/<Directory \/var\/www\/html>/,/<\/Directory>/d' /etc/apache2/sites-available/000-default.conf
-sudo rm -f /etc/apache2/sites-available/000-default.conf.bak
-sudo systemctl restart apache2
-
 # Remove the cloned repository and reset the destination directory
 sudo rm -rf "$destination_dir"
 
