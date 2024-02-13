c8a0d027d905f8114b57ae571e3c11cfdbe448b7
Damianko135 <139293484+Damianko135@users.noreply.github.com>
2024-02-13 15:26:51 +0100

    Update Linux_Uninstall.sh

--- Linux_Uninstall.sh
+++ Linux_Uninstall.sh
@@ -4,9 +4,6 @@
 destination_dir="/var/www/html/"
 Virtual_Host="/var/www/html/Index"
 
-# Remove permissions reset scheduled after 10 minutes
-ps -ef | grep "sudo chmod -R 755 $destination_dir/*" | grep -v grep | awk '{print $2}' | xargs sudo kill
-
 # Undo changes to Apache Virtual Host Configuration
 sudo sed -i "s#DocumentRoot $Virtual_Host#DocumentRoot /var/www/html#" /etc/apache2/sites-available/000-default.conf
 sudo sed -i '/<Directory \/var\/www\/html>/,/<\/Directory>/d' /etc/apache2/sites-available/000-default.conf
