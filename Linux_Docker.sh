534df1447569db0445bf89e77cf425e164961012
Damianko135 <139293484+Damianko135@users.noreply.github.com>
2024-02-20 23:08:59 +0100

    Update Linux_Docker.sh

--- Linux_Docker.sh
+++ Linux_Docker.sh
@@ -5,18 +5,11 @@ sudo apt update
 sudo apt full-upgrade -y
 sudo apt-mark showhold | xargs sudo apt install -y --allow-change-held-packages 
 
-## If you also use a Cloudflare Tunnel, copy the docker code underneath here. it might take a minute, but it'll work
-# Dont forget a -d, do not specify a port for the Tunnel. Thats is why mine didn't work.
-
-
-
-## Clone GitHub repository, in this case mine
-# I cant clone the index, otherwise i would've said Index behind it.
-# The reason i cant do that is that otherwise it doesnt clone the docker-compose.yml file with it.
-git clone main --single-branch https://github.com/Damianko135/Portfoliowebsite.git Index
-
-
-
-
+sudo apt purge containerd.io
+sudo apt autoremove
+sudo apt autoclean
+sudo apt update
+sudo apt install docker.io docker-compose
+sudo systemctl enable docker
 
 
