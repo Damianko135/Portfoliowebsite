e308c886fca76d261f0c3c95e886b8d7feefee25
Damianko135 <139293484+Damianko135@users.noreply.github.com>
2024-02-27 16:01:38 +0100

    Sir

--- Linux_Docker.sh
+++ Linux_Docker.sh
@@ -1,15 +1,29 @@
 #!/bin/bash
 
-## First, Update everything
-sudo apt update 
+# Update package lists
+sudo apt update
+
+# Upgrade installed packages
 sudo apt full-upgrade -y
+
+# Install any held packages
 sudo apt-mark showhold | xargs sudo apt install -y --allow-change-held-packages 
 
-sudo apt purge containerd.io
-sudo apt autoremove
+# Install Docker, Docker Compose, and Git
+sudo apt install docker.io docker-compose git -y
+
+# Remove unnecessary packages
+sudo apt autoremove -y
+
+# Clean up package cache
 sudo apt autoclean
-sudo apt update
-sudo apt install docker.io docker-compose
+
+# Enable Docker service
 sudo systemctl enable docker
 
+# Clone the repository
+git clone https://github.com/Damianko135/Portfoliowebsite.git --single-branch -b main 
 
+cd Portfoliowebsite
+
+docker-compose up -d
