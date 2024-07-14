47484894c71dbc598431d43b9249db34374c3ed6
Damianko135 <139293484+Damianko135@users.noreply.github.com>
2024-07-15 01:12:48 +0200

    Update Linux_Docker.sh

--- Linux_Docker.sh
+++ Linux_Docker.sh
@@ -1,10 +1,61 @@
 #!/bin/bash
 
-## Allow. files, such as.htaccess to also be moved
-shopt -s dotglob
+set -e
 
-## To make sure the commands will be executed correctly.
+# Check if run as root.
+if [ $EUID != 0 ]; then
+    echo "Please run as root"
+    exit 1
+fi
+
+clear
+echo "----- "
+echo " "
+echo " "
+echo " "
+echo "Setting up environment variables..."
+
+# Prompt user for variables
+echo "Please provide the required variables:"
+read -p "Cloudflare token (leave empty for now): " TUNNEL_TOKEN
+
+read -p "Do you want the MySQL database to be available without a password? (Y/n): " EMPTY_PW
+if [[ "$EMPTY_PW" == "N" || "$EMPTY_PW" == "n" ]]; then
+  echo "You will need to set a password on first login."
+else
+  echo "CAUTION: Using MySQL without a password is not recommended for production."
+  EMPTY_PW="yes"
+  await 10
+fi
+
+# Write variables to .env file
+echo "Generating .env file..."
+cat <<EOF > ~/.env
+## Example .env file
+TUNNEL_TOKEN="$TUNNEL_TOKEN"
+MYSQL_ALLOW_EMPTY_PASSWORD="$EMPTY_PW"
+EOF
+
+# Install Docker
+echo "Installing Docker..."
+curl -fsSL https://get.docker.com -o get-docker.sh
+sh get-docker.sh
+rm -f get-docker.sh
+
+# Install Docker Compose
+echo "Installing Docker Compose..."
+curl -fsSL https://github.com/docker/compose/releases/download/1.29.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+chmod +x /usr/local/bin/docker-compose
+
+# Install Git
+echo "Installing Git..."
+apt-get update
+apt-get install -y git
+
+# Clone and setup Portfoliowebsite
+echo "Cloning and setting up Portfoliowebsite..."
 cd ~/
+<<<<<<< Updated upstream
 
 # Update package lists
 sudo apt update
@@ -41,29 +92,30 @@ TUNNEL_TOKEN= " >>./.env
 echo "MYSQL_ALLOW_AMPTY_PASSWORD=" >>./.env
 
 # Start the Portfoliowebsite Docker containers in detached mode
+=======
+git clone https://github.com/Damianko135/Portfoliowebsite.git --single-branch -b main
+mv Portfoliowebsite/docker-compose.yml ~/
+>>>>>>> Stashed changes
 cd ~/ && docker-compose up -d
+cp -rf ~/Portfoliowebsite/Index/* ~/.Portfolio/
+cp -rf ~/Portfoliowebsite/Database/* ~/sql-files/
+rm -rf ~/Portfoliowebsite
 
-# Copy the contents of the Index directory to ~/.Portfolio
-sudo cp -rf ~/Portfoliowebsite/Index/* ~/.Portfolio/
-sudo cp -rf ~/Portfoliowebsite/Database/* ~/sql-files/
+# Clone and setup BBB
+echo "Cloning and setting up BBB..."
+mkdir -p ~/.Block_B && cd ~/.Block_B
+git clone https://github.com/Damianko135/BBB.git
+mv ~/.Block_B/BBB/public/* ~/.Block_B/
+rm -rf ~/.Block_B/BBB
 
-# Remove the Portfoliowebsite directory
-sudo rm -rf ~/Portfoliowebsite
-
-# Clone the BBB repository
-cd ~/.Block_B && sudo git clone https://github.com/Damianko135/BBB.git
-
-# Move the contents of the BBB/public directory to ~/.Block_B
-sudo mv ~/.Block_B/BBB/public/* ~/.Block_B/
-
-# Remove the BBB directory
-sudo rm -rf ~/.Block_B/BBB
-
-# Very nice to have this for seeing the status of all containers.
+# Install Portainer for Docker management
+echo "Installing Portainer for Docker management..."
 docker run -d \
-  -p 9000:9000 \
-  --name portainer \
-  --restart always \
-  -v /var/run/docker.sock:/var/run/docker.sock \
-  -v portainer_data:/data \
-  portainer/portainer-ce:latest
+    -p 9000:9000 \
+    --name portainer \
+    --restart always \
+    -v /var/run/docker.sock:/var/run/docker.sock \
+    -v portainer_data:/data \
+    portainer/portainer-ce:latest
+
+echo "Setup complete."
